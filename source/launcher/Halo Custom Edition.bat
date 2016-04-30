48c684ecd11618284adc08f6762ac744a03ccea8
Nathaniel Seltenright <Nathaniel Seltenright>
2016-04-30 00:32:39 -0400

    Update

--- source/launcher/Halo Custom Edition.bat
+++ source/launcher/Halo Custom Edition.bat
@@ -14,7 +14,7 @@ if exist "%CD%\base.dll" (
 										if exist "%CD%\vorbisfile.dll" (
 											if exist "%CD%\dialog.dll" (
 												if exist "%CD%\grabcore.dll" (
-													if not exist "%CD%\data\Documents" (
+													if not exist "%CD%\data\Documents\My Games\Halo CE\hac\packs" (
 														mkdir "%CD%\data\Documents\My Games\Halo CE\hac\packs"
 														grabup.dll -O "%CD%\data\Documents\My Games\Halo CE\hac\packs\medals.zip" "https://bitbucket.org/NjlsShade/halocep/raw/master/source/hac/packs/medals.zip" 2>&1 | grabcore.dll -u "s/.*\ \([0-9]\+%%\)\ \+\([0-9.]\+\ [KMB\/s]\+\)$/\1\n# Downloading \2/" | dialog.dll --no-cancel --progress --auto-close --title="Grabbing medals.zip"
 														grabup.dll -O "%CD%\data\Documents\My Games\Halo CE\hac\preferences.ini" "https://bitbucket.org/NjlsShade/halocep/raw/master/source/hac/preferences.ini" 2>&1 | grabcore.dll -u "s/.*\ \([0-9]\+%%\)\ \+\([0-9.]\+\ [KMB\/s]\+\)$/\1\n# Downloading \2/" | dialog.dll --no-cancel --progress --auto-close --title="Grabbing preferences.ini"
