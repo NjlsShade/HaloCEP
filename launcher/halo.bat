8f6f50d480263170471e544b00aa9cbbbd711547
NjlsShade <njls.yoshi2@gmail.com>
2019-03-22 00:06:54 -0400

    Patch for Windows 10 1803
    Windows 10 1803 prevented running dll files as excusable.

--- launcher/halo.bat
+++ launcher/halo.bat
@@ -1,34 +1,34 @@
 ::[Bat To Exe Converter]
 ::
 ::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFChcTQCHAE+1BaAR7ebv/Nagq1k1QeADd5neyKeGKeUf7nnodIMj1XJfivcAAxxXMBuoYW8=
+::fBw5plQjdG8=
 ::YAwzuBVtJxjWCl3EqQJgSA==
 ::ZR4luwNxJguZRRnk
 ::Yhs/ulQjdF+5
 ::cxAkpRVqdFKZSTk=
-::cBs/ulQjdF65
+::cBs/ulQjdF+5
 ::ZR41oxFsdFKZSTk=
 ::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpSI=
-::egkzugNsPRvcWATEpSI=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
 ::dAsiuh18IRvcCxnZtBJQ
 ::cRYluBh/LU+EWAnk
 ::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJgZksaGGQ=
+::cxY6rQJ7JhzQF1fEqQJgZkoaHGQ=
 ::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
+::ZQ05rAF9IAHYFVzEqQIfPh5GXwyLOWe+Zg==
 ::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
 ::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
 ::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
+::dhA7uBVwLU+EWHeO+FEROwhRWBfi
 ::YQ03rBFzNR3SWATElA==
 ::dhAmsQZ3MwfNWATElA==
 ::ZQ0/vhVqMQ3MEVWAtB9wSA==
 ::Zg8zqx1/OA3MEVWAtB9wSA==
 ::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFChcTQCHAE+1BaAR7ebv/Nagq1k1QeADd5neyKeGKeUf7nnodIMj1XJfivcDGh5KbRqiawMw52taswQ=
+::Zh4grVQjdDWDJFSB8EsxFAhcTQCHAE+7ErRczPry+e+7gkkcQOMDX4bP8qGMHO8F71f3fZ8o2n5mlMoZAhhQewCXQRkxu31Kv2GJM4mZqwqB
 ::YB416Ek+ZG8=
 ::
 ::
 ::978f952a14a936cc963da21a135fa983
-start "" "./resources/base.dll" -path ./data -window
+start "" "./resources/base.exe" -path ./data
